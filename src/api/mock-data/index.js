75e9a36d2c873ba26b10a1c07d3fc25848fbd6f3
nurlykhan <nurlykhan.salamatuly@samgau.com>
2021-10-25 19:19:47 +0600

    feat: add mock data

---
+++ src/api/mock-data/index.js
@@ -0,0 +1,11 @@
+import fno_101_01 from "./fno_101.json";
+import fno_700 from "./fno_700.json";
+import fno_870 from "./fno_870.json";
+import fno_911 from "./fno_911.json";
+
+export default {
+  fno_101_01,
+  fno_700,
+  fno_870,
+  fno_911,
+};
